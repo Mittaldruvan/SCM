b3e303e881a99572a179ab9bd6bc0b37f909cb18
Druvan <druvan1304.becse24@chitkara.edu.in>
2025-04-09 01:01:26 +0530

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string.h>
 #include <fstream>
-// #include "Appointment.h"
 #include "Patient.h"
 #include "Doctor.h"
 using namespace std;
@@ -14,7 +13,6 @@ main()
     {
         cout << "Press 1 for Calling Patient menu." << "\n";
         cout << "Press 2 for Calling Doctor menu." << "\n";
-        // cout << "Press 3 for Calling Appointment menu." << "\n";
         cout << "Enter number for calling a menu:";
         cin >> i;
         switch (i)
@@ -25,9 +23,6 @@ main()
         case 2:
             getDoctorMenu();
             break;
-        // case 3:
-        //     getAppointmentmenu();
-        //     break;
         }
         cout << "\n"
              << "If u want to call a menu againn(y/n):";
