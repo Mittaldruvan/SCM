41e71ee76134bbe900f9fbd0e212515899829701
Druvan <druvan1304.becse24@chitkara.edu.in>
2025-04-09 00:26:07 +0530

    final file of project added

---
+++ Project.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+// #include "Appointment.h"
+#include "Patient.h"
+#include "Doctor.h"
+using namespace std;
+main()
+{
+
+    int i;
+    char a;
+    do
+    {
+        cout << "Press 1 for Calling Patient menu." << "\n";
+        cout << "Press 2 for Calling Doctor menu." << "\n";
+        // cout << "Press 3 for Calling Appointment menu." << "\n";
+        cout << "Enter number for calling a menu:";
+        cin >> i;
+        switch (i)
+        {
+        case 1:
+            getPatientMenu();
+            break;
+        case 2:
+            getDoctorMenu();
+            break;
+        // case 3:
+        //     getAppointmentmenu();
+        //     break;
+        }
+        cout << "\n"
+             << "If u want to call a menu againn(y/n):";
+        cin >> a;
+    } while (a == 'y');
+}
